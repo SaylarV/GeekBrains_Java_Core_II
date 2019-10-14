1c244099c7d38a75eb042b93e4373dde95b2e53e
SaylarV <vazhuravlevwork@gmail.com>
2019-10-14 14:07:45 +0300

    update HW 3

--- Lessons/src/GeekBrains_HW_Lesson_3/FruitList.java
+++ Lessons/src/GeekBrains_HW_Lesson_3/FruitList.java
@@ -1,45 +1,29 @@
 package GeekBrains_HW_Lesson_3;
-//1 Создать массив с набором слов (10-20 слов, должны встречаться повторяющиеся). Найти и
-//вывести список уникальных слов, из которых состоит массив (дубликаты не считаем).
-//Посчитать, сколько раз встречается каждое слово.
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
-public class FruitList {
+class FruitList {
+    private List<String> fruitList = new ArrayList<>();
 
-    private List<String> list = new ArrayList<>();
-
-    void add(String name){
-        list.add(name);
+    FruitList() {
     }
 
-    void printList(){
-        add("Banana");
-        add("Banana");
-        add("Apple");
-        add("Kiwi");
-        add("Banana");
-        add("Coconut");
-        add("Melon");
-        add("Apple");
-        add("Strawberry");
-        add("Blueberry");
-        add("Coconut");
-        System.out.print("Unique words: ");
-        for (int i = 0; i < list.size(); i++){
+    void setFruitList(List<String> fruitList) {
+        this.fruitList = fruitList;
+    }
+
+    void countFruits(){
+        Map<String, Integer> uniqFruits = new HashMap<>();
+        for(int i = 0; i < fruitList.size(); i++){
             int count = 1;
-            for (int j = 0; j < list.size(); j++) {
-                if (list.get(i).equals(list.get(j))) {
-                    if (i!=j){
-                        count++;
-                    }
+            for (String s : fruitList) {
+                if (fruitList.get(i).equals(s)) {
+                    uniqFruits.put(fruitList.get(i), count);
+                    count++;
                 }
             }
-            if (count==1){
-                System.out.print(list.get(i)+" ");
-            }
         }
-        System.out.println();
+        uniqFruits.forEach((k,v) -> {System.out.println(k + " " + v);});
     }
+
 }
--- Lessons/src/GeekBrains_HW_Lesson_3/Phonebook.java
+++ Lessons/src/GeekBrains_HW_Lesson_3/Phonebook.java
@@ -1,42 +1,30 @@
 package GeekBrains_HW_Lesson_3;
-//2 Написать простой класс Телефонный Справочник, который хранит в себе список фамилий и
-//телефонных номеров. В этот телефонный справочник с помощью метода add() можно
-//добавлять записи, а с помощью метода get() искать номер телефона по фамилии. Следует
-//учесть, что под одной фамилией может быть несколько телефонов (в случае однофамильцев),
-//тогда при запросе такой фамилии должны выводиться все телефоны. Желательно не
-//добавлять лишний функционал (дополнительные поля (имя, отчество, адрес), взаимодействие с
-//пользователем через консоль и т.д). Консоль использовать только для вывода результатов проверки
-//телефонного справочника.
 
 import java.util.HashMap;
 import java.util.Map;
 
 class Phonebook {
-    //private String phoneNumber;
-    private String lastName;
 
-    Phonebook(String lastName) {
-        this.lastName = lastName;
+    private Map<String,String> phonebook = new HashMap<>();
+
+    Phonebook() {
     }
 
-    private Map<String,String> phoneBook = new HashMap<>();
-
-    private void add(String phoneNumber, String lastName){
-        phoneBook.put(phoneNumber,lastName);
+    void add(String number, String name){
+        phonebook.put(number,name);
     }
-
-    void get(){
-        add("123456","Sidorov");
-        add("234567","Petrov");
-        add("345678","Ivanov");
-        add("456789","Sidorov");
-
-        phoneBook.forEach((key,value) ->{
-            if (value.equals(lastName)){
-                System.out.println(value + "'s phone number is " + key);
+    void get(String name){
+        phonebook.forEach((k,v) ->{
+            if (v.equals(name)){
+                System.out.println(v + "'s phone number is " + k);
             }
         });
     }
+
+    void printAll(){
+        phonebook.forEach((k,v) ->{System.out.println(k + " " + v);});
+    }
+
 }
 
 
